424333d682e4b6efdf1fc866fe0e91e104cc8254
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-05-26 14:09:54 -0600

    13 queries added for the two new tables

---
+++ 4_queries/9_cohort_avg_assist_timq.sql
@@ -0,0 +1,18 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM (SELECT cohorts.name as cohort, SUM(started_at - created_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohort
+ORDER BY total_duration) as ThisTable;
+
+
+-- SELECT avg (total_duration) as average_total_duration
+-- FROM (
+--   SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+--   FROM assistance_requests
+--   JOIN students ON students.id = student_id
+--   JOIN cohorts on cohorts.id = cohort_id
+--   GROUP BY cohorts.name
+--   ORDER BY total_duration
+-- ) as total_durations;
