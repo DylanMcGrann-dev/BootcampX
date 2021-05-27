5a2d45bd23cf4e000aeced6493dee341dad116ca
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-05-27 12:14:49 -0600

    node postgres now protected against SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,17 @@
 const { Pool } = require('pg');
-
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers 
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+LIMIT $2;
+`;
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -8,15 +20,7 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers 
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv.slice[2]}'
-ORDER BY teacher;
-`)
+pool.query(queryString,values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
